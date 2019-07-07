975b8a8471836514253515f67d60fee4e513f24f
wfsovereign <wfsovereign@outlook.cm>
2019-07-07 21:56:25 +0800

    2.1 - 2.3 notes

--- src/2019-07-07.py
+++ src/2019-07-07.py
@@ -1,2 +1,54 @@
 
 print("hello world!")
+
+message = "hello world"
+
+print(message)
+
+message = "hello python world"
+
+print(message)
+
+print(message.title())
+
+print("hello \nworld \n \t multiple space")
+
+print("   space".rstrip())
+print("   space")
+print("space   ")
+print("space   ".rstrip())
+
+text = "  space"
+
+print(text)
+print(text.rstrip())
+print(text.strip())
+
+# 2.3 2-3
+customerName = 'john'
+print("hello " + customerName + ", would you like to learn some python today?")
+
+# 2.3 2-4
+someoneName = "john snow"
+print(someoneName.title())
+print(someoneName.lower())
+print(someoneName.upper())
+
+# 2.3 2-5
+
+print('Schrödinger once said: "Life feeds on negative entropy"')
+
+# 2.3 2-6
+
+famousPerson = "Schrödinger"
+famousPersonWords = "Life feeds on negative entropy"
+print(famousPerson + ' once said: "' + famousPersonWords + '"')
+
+
+# 2.3 2-7
+
+famousPerson = "\tSchrödinger \n"
+
+print(famousPerson)
+print(famousPerson.lstrip())
+print(famousPerson.strip())
