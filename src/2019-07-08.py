ac68a6d64dd22d41dcb488e4df051a22a4868532
wfsovereign <wfsovereign@outlook.cm>
2019-07-09 00:11:38 +0800

    add today learn notes

---
+++ src/2019-07-08.py
@@ -0,0 +1,59 @@
+
+print(2 * 3)
+
+print(0.1 + 0.2)
+
+print(1 + 2)
+
+print(2 * 8)
+
+
+# print('happy ' + 23 + 'rd birthday')
+print('happy ' + str(23) + 'rd birthday')
+
+
+# 2.4 2-8
+
+print(4 + 4)
+print(7 - -1)
+print(2 * 4)
+print(16 / 2)
+
+print(int(32 / 4))
+
+
+# 2.4 2-9
+favoriteNumber = '9'
+print('my favoriate number is ' + str(favoriteNumber))
+
+'''
+Mutiple annotation
+'''
+
+
+'''
+import this
+
+>>> import this
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
