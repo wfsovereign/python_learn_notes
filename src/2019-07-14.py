f5b390b358f6b49b89d507b43e744d2ba11ed4c2
wfsovereign <wfsovereign@outlook.cm>
2019-07-14 23:41:51 +0800

    today notes

---
+++ src/2019-07-14.py
@@ -0,0 +1,22 @@
+
+
+dimensions = (9, 10)
+
+print(dimensions)
+print(dimensions[0])
+print(dimensions[1])
+
+dimensions = (80, 100)
+
+print(dimensions)
+
+foods = ('meat', 'milk', 'eggs', 'bread', 'beer')
+
+# print('foods ,', foods)
+
+for food in foods: 
+  print(food)
+
+foods = ('vegetables', 'soybean-milk', 'eggs', 'bread', 'beer')
+
+print(foods)
