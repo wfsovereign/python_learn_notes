fa1bf9164dc4c728ca29b43471818fb0de97c8e7
wfsovereign <wfsovereign@outlook.cm>
2019-07-16 00:09:56 +0800

    notes

---
+++ src/2019-07-15.py
@@ -0,0 +1,39 @@
+
+
+print('hello' == "hello")
+
+print('Hello'.lower() == 'hello')
+
+print(8 > 0)
+
+print(9 > 8)
+
+print(9 > 9)
+
+print(9 > 8 and 8 >= 4)
+
+print('hello' in ['hello', 'none', 'yollow'])
+
+
+
+alienColor = ['green', 'red', 'yellow']
+
+alien = 'red'
+
+if alien == 'green':
+  print('get 5 points')
+else:
+  print('get 10 points')
+
+
+if alien == 'green':
+  print('get 5 points')
+elif alien == 'yellow':
+  print('get 10 points')
+elif alien == 'red':
+  print('get 15 points')
+
+  
+
+
+
