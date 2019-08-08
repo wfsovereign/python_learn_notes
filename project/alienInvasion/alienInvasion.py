4d586c762dd0ce9b6c3a2e468731ae16510d6e28
wfsovereign <wfsovereign@outlook.cm>
2019-08-08 22:10:39 +0800

    move ship

--- project/alienInvasion/alienInvasion.py
+++ project/alienInvasion/alienInvasion.py
@@ -1,9 +1,8 @@
 import pygame
-import sys
 
 from settings import Settings
 from ship import Ship
-
+import game_function as gf
 
 def run_game() :
   pygame.init()
@@ -11,16 +10,13 @@ def run_game() :
   screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
 
-  ship = Ship(screen)
+  ship = Ship(ai_settings, screen)
 
 
   while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
-    pygame.display.flip()
+    gf.check_events(ship)
+    ship.update()
+    gf.update_screen(ai_settings, screen, ship)
 
 run_game()
 
--- project/alienInvasion/settings.py
+++ project/alienInvasion/settings.py
@@ -8,3 +8,4 @@ class Settings():
     self.screen_height = 800
     self.bg_color = (230, 230, 200)
     
+    self.ship_speed_factor = 1.5
