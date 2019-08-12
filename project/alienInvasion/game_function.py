15346b84cc6e75ba853edf39fc0f587ca9057bfe
wfsovereign <wfsovereign@outlook.cm>
2019-08-12 22:46:04 +0800

    13.3.4, create alien and refactor

--- project/alienInvasion/game_function.py
+++ project/alienInvasion/game_function.py
@@ -3,6 +3,7 @@ import sys
 import pygame 
 
 from bullet import Bullet
+from alien import Alien
 
 
 def check_events(ai_settings, screen, ship, bullets):
@@ -21,6 +22,8 @@ def check_keyup_events(event, ship):
     ship.moving_right = False
   elif event.key == pygame.K_LEFT:
     ship.moving_left = False
+  elif event.key == pygame.K_q:
+    sys.exit()
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
   if event.key == pygame.K_RIGHT:
@@ -30,11 +33,13 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
   elif event.key == pygame.K_SPACE:
     fire_bullet(ai_settings, screen, ship, bullets)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
   screen.fill(ai_settings.bg_color)
   for bullet in bullets.sprites():
     bullet.draw_bullet()
   ship.blitme()
+  aliens.draw(screen)
+
   pygame.display.flip()
 
   
@@ -52,3 +57,29 @@ def fire_bullet(ai_settings, screen, ship, bullets):
   if len(bullets) < ai_settings.bullet_allowed:
     new_bullet = Bullet(ai_settings, screen, ship)
     bullets.add(new_bullet)
+
+
+def create_fleet(ai_settings, screen, aliens):
+
+  alien = Alien(ai_settings, screen)
+  alien_width = alien.rect.width
+
+  number_alien_x = get_number_aliens_x(ai_settings, alien_width)
+
+  for alien_number in range(number_alien_x):
+    create_alien(ai_settings, screen, aliens, alien_number, alien_width)
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+  available_space_x = ai_settings.screen_width - 2 * alien_width
+  number_alien_x = int(available_space_x / (2 * alien_width))
+
+  return number_alien_x
+
+def create_alien(ai_settings, screen, aliens, alien_number, alien_width):
+    alien = Alien(ai_settings, screen)
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+  
+  
