80e89e8cb4f97af76dc497b516b6c20df3e2e989
wfsovereign <wfsovereign@outlook.cm>
2019-08-13 22:37:06 +0800

    can shot alien

--- project/alienInvasion/game_function.py
+++ project/alienInvasion/game_function.py
@@ -44,13 +44,15 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 
   
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
 
   bullets.update()
 
   for bullet in bullets.copy():
     if bullet.rect.bottom <= 0:
       bullets.remove(bullet)
+
+  collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
@@ -94,3 +96,19 @@ def get_number_rows(ai_settings, ship_height, alien_height):
   return number_rows
 
 
+def update_aliens(ai_settings, aliens):
+
+  check_fleet_edges(ai_settings, aliens)
+  aliens.update()
+
+
+def check_fleet_edges(ai_settings, aliens):
+  for alien in aliens.sprites():
+    if alien.check_edges():
+      change_fleet_direction(ai_settings, aliens)
+      break
+
+def change_fleet_direction(ai_settings, aliens):
+  for alien in aliens.sprites():
+    alien.rect.y += ai_settings.fleet_drop_speed
+  ai_settings.fleet_direction *= -1
