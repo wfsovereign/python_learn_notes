88eaba309dbb2b3e33572052e3dc9afc4e1bc247
wfsovereign <wfsovereign@outlook.cm>
2019-08-15 23:38:51 +0800

    reset and update game level

--- project/alienInvasion/game_function.py
+++ project/alienInvasion/game_function.py
@@ -26,6 +26,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
   button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
 
   if button_clicked and not stats.game_active:
+    ai_settings.initialize_dynamic_settings()
     pygame.mouse.set_visible(False)
     stats.reset_stats()
     stats.game_active = True
@@ -84,6 +85,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
   if len(aliens) == 0:
 
     bullets.empty()
+    ai_settings.increase_speed()
     create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
--- project/alienInvasion/settings.py
+++ project/alienInvasion/settings.py
@@ -11,15 +11,30 @@ class Settings():
     self.ship_speed_factor = 8
     self.ship_limit = 2
 
-    self.bullet_speed_factor = 16
     self.bullet_width = 8
     self.bullet_height = 15
     self.bullet_color = 60,60,60
     self.bullet_allowed = 30
     self.bullet_awesome = True
 
-    self.alien_speed_factor = 20
+
     self.fleet_drop_speed = 10
 
     self.fleet_direction = 1
 
+    self.speedup_scale = 1.3
+
+    self.initialize_dynamic_settings()
+
+
+  def initialize_dynamic_settings(self):
+
+    self.ship_speed_factor = 10
+    self.bullet_speed_factor = 16
+    self.alien_speed_factor = 20
+    self.fleet_direction = 1
+
+  def increase_speed(self):
+    self.ship_speed_factor *= self.speedup_scale
+    self.bullet_speed_factor *= self.speedup_scale
+    self.alien_speed_factor *= self.speedup_scale
